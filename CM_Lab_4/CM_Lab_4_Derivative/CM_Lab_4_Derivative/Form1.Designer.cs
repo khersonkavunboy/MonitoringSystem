592485410fd131c9e961332f1fc9bd78751ccf3c
khersonkavunboy <57249060+khersonkavunboy@users.noreply.github.com>
2019-11-01 09:28:48 +0200

    Test
    Test

---
+++ CM_Lab_4/CM_Lab_4_Derivative/CM_Lab_4_Derivative/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CM_Lab_4_Derivative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.derivative1_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.derivative1_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // derivative1_chart
+            // 
+            this.derivative1_chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.derivative1_chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.derivative1_chart.Legends.Add(legend1);
+            this.derivative1_chart.Location = new System.Drawing.Point(226, 12);
+            this.derivative1_chart.Name = "derivative1_chart";
+            series1.BorderWidth = 12;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Color = System.Drawing.Color.Navy;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.Black;
+            series1.MarkerColor = System.Drawing.Color.Black;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Аналитические точки";
+            series2.BorderWidth = 10;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.MediumSpringGreen;
+            series2.LabelBorderWidth = 10;
+            series2.Legend = "Legend1";
+            series2.MarkerColor = System.Drawing.Color.Red;
+            series2.Name = "Вычисленные значения";
+            this.derivative1_chart.Series.Add(series1);
+            this.derivative1_chart.Series.Add(series2);
+            this.derivative1_chart.Size = new System.Drawing.Size(1136, 472);
+            this.derivative1_chart.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Первая центральная разность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 67);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вторая центральная разность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 496);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.derivative1_chart);
+            this.Name = "Form1";
+            this.Text = "D E R I V A T I V  E  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.derivative1_chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart derivative1_chart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CM_Lab_4/CM_Lab_4_Derivative/CM_Lab_4_Derivative/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CM_Lab_4_Derivative
+{
+    public partial class Form1 : Form
+    {
+        // но что-то вроде «задана функция -> найти 1 и 2 производную ->
+        // построить 2 графика(с центральной разницей и аналитический)»
+        
+        public Form1()
+        {
+            InitializeComponent();
+            a = -2;
+            b = 1;
+            step = 0.01;
+        }
+
+        private void Form1_Load(object sender, EventArgs handler)
+        {
+
+        }
+        private double Function_x3sinx_1(double x) {
+
+            return Math.Sin(x*x+1);
+        }
+        ///--------------
+        private double FirstDerivative(double f,double h) {
+            
+            return (Function_x3sinx_1(f+h) - Function_x3sinx_1(f-h)) / (2 * h);
+        }
+
+        private double SecondDerivative(double f, double h)
+        {
+            return (Function_x3sinx_1(f - h) - 2 * Function_x3sinx_1(f) + Function_x3sinx_1(f + h)) / (h * h);
+        }
+
+
+        double a;
+        double b;
+        double step;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < derivative1_chart.Series.Count; i++)
+            {
+                derivative1_chart.Series[i].Points.Clear();
+            }
+            
+            double x = a;
+            
+            double RightDifference = (Function_x3sinx_1(x + step) - Function_x3sinx_1(x)) / step;
+            derivative1_chart.Series[0].Points.AddXY(x, Function_x3sinx_1(x));
+            derivative1_chart.Series[1].Points.AddXY(x, RightDifference);
+            x += step;
+
+            while (x < b)
+            {
+                derivative1_chart.Series[0].Points.AddXY(x, 2*x*Math.Cos(Math.Pow(x,2)+1));
+                derivative1_chart.Series[1].Points.AddXY(x, FirstDerivative(x, step));
+                x += step;
+            }
+
+           double LeftDifference = (Function_x3sinx_1(x) - Function_x3sinx_1(x - step)) / step;
+           derivative1_chart.Series[0].Points.AddXY(x, 2 * x * Math.Cos(Math.Pow(x, 2) + 1));
+           derivative1_chart.Series[1].Points.AddXY(x, LeftDifference);
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < derivative1_chart.Series.Count; i++)
+            {
+                derivative1_chart.Series[i].Points.Clear();
+            }
+
+            double x = a;
+            while (x <= b)
+            {
+                derivative1_chart.Series[0].Points.AddXY(x,2*Math.Cos(Math.Pow(x,2)+1) - 4*Math.Pow(x,2) *Math.Sin(Math.Pow(x,2)+1));
+                derivative1_chart.Series[1].Points.AddXY(x, SecondDerivative(x, step));
+                x += step;
+            }
+        }
+        
+
+   
+       
+
+      
+    }
+}
